d46f75891720bd0c7ccd8cfbeec90e1b2d90085c
rin <springephemeral1123@gmail.com>
2019-05-10 17:38:40 +0900

    update

--- Python/docker-django/django/my_app/settings.py
+++ Python/docker-django/django/my_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'my_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',  #作成時のデフォルト
+        'USER': 'postgres',  #作成時のデフォルト
+        'PASSWORD' : 'foobar', #作成時にdocker-compose.ymlで設定
+        'HOST' : 'postgres', #コンテナのサーバ名
+        'PORT' : 5432,
     }
 }
 
@@ -118,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/opt/static'
